beb4ad93bac7610e34538d3b2feeeb452abd8e4a
docio <docio@debian>
2016-08-31 15:17:17 -0400

    removed most unused members of the union variable in a test of union. the change is caused partly by a compilation note: \n the ABI of passing union with long double has changed in GCC 4.4

--- tests/test_union_0.c
+++ tests/test_union_0.c
@@ -10,32 +10,6 @@
 union my_union_1{
   int member_1;
   char member_2;
-  signed char member_3;
-  unsigned char member_4;
-  short member_5;
-  short int member_6;
-  signed short member_7;
-  signed short int member_8;
-  unsigned short int member_9;
-  signed member_10;
-  signed int member_11;
-  unsigned member_12;
-  unsigned int member_13;
-  long member_14;
-  long int member_15;
-  signed long member_16;
-  signed long int member_17;
-  unsigned long member_18;
-  unsigned long int member_19;
-  long long member_20;
-  long long int member_21;
-  signed long long member_22;
-  signed long long int member_23;
-  unsigned long long member_24;
-  unsigned long long int member_25;
-  float member_26;
-  double member_27;
-  long double member_28;
 };
 
 FILE * fp;
