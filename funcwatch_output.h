1ee8c308c26658fb28864c46b046b1e3c1f683e4
Siyuan Jiang <sjiang1@nd.edu>
2016-09-15 11:28:11 -0400

    Added a macro define protection to funcwatch_output.h

--- funcwatch_output.h
+++ funcwatch_output.h
@@ -1,4 +1,9 @@
+#ifndef _FUNCWATCH_OUTPUT_
+#define _FUNCWATCH_OUTPUT_
+
 #include <stdio.h>
 #include "funcwatch.h"
 
 void output_logged_values(FILE *f, funcwatch_run *run);
+
+#endif // _FUNCWATCH_OUTPUT_
