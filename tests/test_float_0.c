e1f51c06c1db3f3bfcbc14299a8cd6af2261f5a9
Siyuan Jiang <sjiang1@nd.edu>
2016-10-28 17:43:27 -0400

    Updated the tests.

--- tests/test_float_0.c
+++ tests/test_float_0.c
@@ -6,13 +6,13 @@
 #include <stdio.h>
 #include <unistd.h>
 
-float foo(int parameter_1){
+float foo(float parameter_1){
   float a = parameter_1 * 0.001;
   return a;
 }
 
 void main(int argc, char *argv[]) {
-  int arg1 = -1;
+  float arg1 = -2.1;
   
   float ret = foo(arg1);
 }
