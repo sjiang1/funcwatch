ccfbca2659fc81c7f84c1ac367d81680d1d73045
Siyuan Jiang <sjiang1@nd.edu>
2016-11-13 21:07:22 -0500

    Updated a test.

--- test_vectorappend.c
+++ test_vectorappend.c
@@ -9,11 +9,10 @@ int main(int argc, char *argv[]) {
   // fill it up with 150 arbitrary values
   // this should expand capacity up to 200
   int i;
-  for (i = 200; i > -50; i--) {
+  for (i = 2; i > 0; i--) {
     int *data = (int *) malloc(sizeof(int));
     *data = i;
     vector_append(&vector, data);
-    i = i - 50;
   }
 
   // we're all done playing with our vector,
